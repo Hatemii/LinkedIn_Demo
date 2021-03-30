253ce588f0636f60dc4a5a77420640a7772dfcc8
Hatemii <hatem.xhezairi96@gmail.com>
2021-03-30 21:17:41 +0200

    Jobs CRUD -> with authentication

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,63 @@
+class JobsController < ApplicationController
+  before_action :set_job, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:show,:index]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+
+    respond_to do |format|
+      if @job.save
+        format.html { redirect_to @job, notice: "Job was successfully created." }
+        format.json { render :show, status: :created, location: @job }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @job.update(job_params)
+        format.html { redirect_to @job, notice: "Job was successfully updated." }
+        format.json { render :show, status: :ok, location: @job }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @job.destroy
+    respond_to do |format|
+      format.html { redirect_to jobs_url, notice: "Job was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_job
+      @job = Job.find(params[:id])
+    end
+
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
+end
